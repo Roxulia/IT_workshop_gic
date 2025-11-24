e7339185bc197e7d783a7c459a0d165c0d410898
zinlinhtet-gic <zinlinhtet@gicjp.com>
2025-11-24 13:26:44 +0630

    Add Day 25 Tasks

---
+++ day25/src/test/java/com/gicws/day25/Day25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gicws.day25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
